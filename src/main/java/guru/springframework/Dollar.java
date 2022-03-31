605035a56c51940b528880f57cdf589d0e737307
Ceci <carbel.cecilia@gmail.com>
2022-03-31 11:58:26 +0200

    adding changes class Monney to class Dollar and Franc

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,18 +1,16 @@
 package guru.springframework;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money{
+
     public Dollar(int amount) {
         this.amount = amount;
     }
+
     Dollar times(int multiplier){
         //amount = amount * multiplier;
         //amount *= multiplier;(refactor)
         return new Dollar(amount * multiplier);
     }
 
-    public boolean equals(Object object){
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
+
 }
---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+//generate class Money to link dollar and frac class
+//se pasa  data type a protected
+public class Money {
+    protected int amount;//class protected to access both Dollar and Franc by the data type that now parent class Money gives them
+    public boolean equals(Object object){
+        Money money = (Money) object;//change Dollar and Franc for Money, inherits from father
+        return amount == money.amount;
+    }
+}

fc8e33041faebf54fb1fcd048e04c792b6d733b4
Ceci <carbel.cecilia@gmail.com>
2022-04-01 00:58:16 +0200

    implementacion and equals

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -5,6 +5,7 @@ public class Money {
     protected int amount;//class protected to access both Dollar and Franc by the data type that now parent class Money gives them
     public boolean equals(Object object){
         Money money = (Money) object;//change Dollar and Franc for Money, inherits from father
-        return amount == money.amount;
+        return amount == money.amount
+                && this.getClass().equals(object.getClass());//
     }
 }
